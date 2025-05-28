456bc271ea5a6a142cdc45220074351a3804da52
YoussefGri <yyoussef781@gmail.com>
2025-05-28 14:34:46 +0200

    reg

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
    //apiUrl: 'http://localhost:8000',
-   apiUrl: '   https://karabs-backend.up.railway.app',
+   apiUrl: '   https://karabs-backend1.up.railway.app',
    mapsKey: 'AIzaSyAW5_fqzhBzXO1pNDjxg4H02SNCVIQ4ej8',
   };
