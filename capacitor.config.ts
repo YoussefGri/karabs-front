e93560f82e409a286f2a18ba74560f922f175e2a
YoussefGri <yyoussef781@gmail.com>
2025-05-30 21:05:35 +0200

    minor fixes for oauth

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,38 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+// import type { CapacitorConfig } from '@capacitor/cli';
+
+// const config: CapacitorConfig = {
+//   appId: 'io.ionic.starter',
+//   appName: 'krabs-front',
+//   webDir: 'www'
+// };
+
+// export default config;
+
+import type { CapacitorConfig } from "@capacitor/cli"
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'krabs-front',
-  webDir: 'www'
-};
+  appId: "io.ionic.starter",
+  appName: "krabs-front",
+  webDir: "www",
+  server: {
+    androidScheme: "https",
+    // Autoriser la navigation vers votre nouveau domaine
+    allowNavigation: ["karabs-front.vercel.app", "karabs-backend1.up.railway.app", "localhost", "127.0.0.1"],
+  },
+  plugins: {
+    Browser: {
+      androidBrowserMode: "webview",
+      enableJavaScript: true,
+    },
+    App: {
+      appUrlOpen: {
+        enabled: true,
+      },
+    },
+  },
+}
 
-export default config;
+export default config
+
+
+ 
