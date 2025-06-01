c7a2271020d871954e23796163a0e9eaae8b0e1f
YoussefGri <yyoussef781@gmail.com>
2025-06-01 13:47:15 +0200

    capacitor conflict 2

--- android/app/src/main/java/com/karabs/acm/MainActivity.java
+++ android/app/src/main/java/com/karabs/acm/MainActivity.java
@@ -1,14 +1,5 @@
 package com.karabs.acm;
 
 import com.getcapacitor.BridgeActivity;
-import android.os.Bundle;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
-import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Initialize GoogleAuth plugin
-        registerPlugin(GoogleAuth.class);
-    }
-}
+public class MainActivity extends BridgeActivity {}
